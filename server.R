457313afa3bd47011a0b53cde5e211210a60f3d4
Ruslan Al-Ali <ruslan.alali@gmail.com>
2019-06-28 13:51:45 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+install.packages("ggplot2")
 library(ggplot2)
 tsneY = read.csv(file = "TSNE_scATAC.csv",row.names = 1)
 our_small_peaky=read.csv(file = "colData.csv",row.names = 1)
@@ -9,7 +10,7 @@ shinyServer(function(input, output,session) {
   
   output$myPlot<- renderPlot({
     tsize=input$dotSize
-    print(tsize)
+#    print(tsize)
     ggplot(graphy,aes(X,Y,col=factor(sample)))+
       scale_colour_manual(name = "samples",values = c("brown3","brown2","cadetblue3"))+
       theme_classic()+
