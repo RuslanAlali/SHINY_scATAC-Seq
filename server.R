68a6685c3f7398905732b54d47cdf481745c24eb
Ruslan Al-Ali <ruslan.alali@gmail.com>
2019-06-28 22:01:33 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -5,20 +5,21 @@ shinyServer(function(input, output,session) {
   
   output$myPlot<- renderPlot({
     #library(ggplot2)
-    our_small_peaky=read.csv(file = "colData.csv",row.names = 1)
-    tsneY = read.csv(file = "TSNE_scATAC.csv",row.names = 1)
+    #our_small_peaky=read.csv(file = "colData.csv",row.names = 1)
+    #tsneY = read.csv(file = "TSNE_scATAC.csv",row.names = 1)
     
-    graphy= data.frame(tsneY,our_small_peaky)
-    colnames(graphy)=c("X","Y","type","sample")
+    #graphy= data.frame(tsneY,our_small_peaky)
+    #colnames(graphy)=c("X","Y","type","sample")
     
-    tsize=input$dotSize
+    #tsize=input$dotSize
 #    print(tsize)
     #ggplot(graphy,aes(X,Y,col=factor(sample)))+
      # scale_colour_manual(name = "samples",values = c("brown3","brown2","cadetblue3"))+
       #theme_classic()+
       #geom_point(size=tsize)
-    plot(graphy$X,graphy$Y, cex=tsize,col=graphy$sample,pch=19)
-  }
+    #plot(graphy$X,graphy$Y, cex=tsize,col=graphy$sample,pch=19)
+  hist(1:10000)
+    }
     
   )
   
