1ee25d694b675abfba9896e9c8672a6a789a59ae
Ruslan Al-Ali <ruslan.alali@gmail.com>
2019-06-28 14:52:27 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -1,12 +1,12 @@
 #install.packages("ggplot2")
-#library(ggplot2)
-tsneY = read.csv(file = "TSNE_scATAC.csv",row.names = 1)
-our_small_peaky=read.csv(file = "colData.csv",row.names = 1)
-graphy= data.frame(tsneY,our_small_peaky)
-colnames(graphy)=c("X","Y","type","sample")
 
 
 shinyServer(function(input, output,session) {
+  library(ggplot2)
+  tsneY = read.csv(file = "TSNE_scATAC.csv",row.names = 1)
+  our_small_peaky=read.csv(file = "colData.csv",row.names = 1)
+  graphy= data.frame(tsneY,our_small_peaky)
+  colnames(graphy)=c("X","Y","type","sample")
   
   output$myPlot<- renderPlot({
    # tsize=input$dotSize
