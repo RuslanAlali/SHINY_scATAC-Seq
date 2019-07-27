5dad1934c4c0611f747d6e9be9c0bd6a8be3c07e
Ruslan Al-Ali <ruslan.alali@gmail.com>
2019-07-28 01:29:26 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -2,49 +2,69 @@
 #library(shiny)
 #runApp()
 #install.packages("png")
+#if (!require("pacman")) install.packages("pacman")
+
 
 shinyServer(function(input, output,session) {
+  
   library(ggplot2)
-  our_small_peaky=read.csv(file = "colData.csv",row.names = 1)
-  tsneY = read.csv(file = "TSNE_scATAC.csv",row.names = 1)
+  library(pacman)
+  library(grid)
+  library(png)
+  
+  our_small_peaky=read.csv(file = "data/colData.csv",row.names = 1)
+  tsneY = read.csv(file = "data/TSNE_scATAC.csv",row.names = 1)
   
   graphy= data.frame(tsneY,our_small_peaky)
   colnames(graphy)=c("X","Y","type","sample")
   
   Ploting=NULL
-  
-  plotReactive<-reactive({
+  image <- png::readPNG("img/background.png")
+
+#-----------
+#Prepare the plots
+#-----------
+    plotReactive<-reactive({
     tsize=input$dotSize
+    
     if (input$Col_group_radio=="sample_id"){
       plot1=ggplot(graphy,aes(X,Y,col=factor(sample)))+
-        scale_colour_manual(name = "By sample               .",values = c("brown4","brown2","coral3","cadetblue3","cadetblue4"), labels = c("Astro1","Astro2","Astro3","Oligo1","Oligo2"))+
+        scale_colour_manual(values = c("brown4","brown2","coral3","cadetblue3","cadetblue4"), labels = c("Astro1","Astro2","Astro3","Oligo1","Oligo2"),name = "By sample               .")+
         theme_classic()+
         geom_point(size=tsize)
-    }else if (input$Col_group_radio=="type_id"){
+    
+      }else if (input$Col_group_radio=="type_id"){
       plot1=ggplot(graphy,aes(X,Y,col=factor(type)))+
+        theme(plot.title = element_text("Explorer for sc-ATAC-Seq low grade glioma tumours"))+
         scale_colour_manual(name = "Tumor type",values = c("brown4","cadetblue4"), labels = c("Astrocytoma","Oligodendroglioma"))+
         theme_classic()+
         geom_point(size=tsize)
       
     }else{
       plot1 <-  ggplot(graphy,aes(X,Y,col=factor(type)))+
+        theme(plot.title = element_text("Explorer for sc-ATAC-Seq low grade glioma tumours", size=14, face="bold.italic"))+
         scale_colour_manual(name = "Tumor type",values = c("brown4","cadetblue4"), labels = c("Astrocytoma","Oligodendroglioma"))+
         theme_classic()+
         geom_point(size=tsize)
     } 
+    
+    # Add photo background
+    if (input$background) {plot1=plot1+ annotation_custom(rasterGrob(image, width = unit(1,"npc"), height = unit(1,"npc")), -Inf, Inf, -Inf, Inf) }else{plot1=plot1}
   })
-  
-  
+
+#-----------
+#Render plot
+#-----------
   output$myPlot<- renderPlot({
       print(plotReactive())
     }  )
   
-
-
-  output$download_image<- downloadHandler(        filename = function() { paste('plot', '.png', sep='') },
-                                                  content=function(file){
-                                                    png(file)
-                                                    ggsave(plot= print(plotReactive()) ,filename = file,  width = 9,height = 4.5, units = "in")
-                                                     }) 
+#-----------
+#Download image
+#-----------
+    output$download_image<- downloadHandler(  filename = function() { paste('plot', '.png', sep='') },
+                                              content=function(file){
+                                              png(file)
+                                              ggsave(plot= print(plotReactive()) ,filename = file, dpi = 300, width = 12,height = 6, units = "in") }) 
   
 })
--- ui.R
+++ ui.R
@@ -9,62 +9,35 @@ shinyServer(
                #tabPanel("Component 3")
     ),
     
-    headerPanel  (h2("Explorer for sc-ATAC-Seq low grade glioma tumours", align="center")),
+    headerPanel  (h2("Explorer for sc-ATAC-Seq low grade glioma tumours")),
     headerPanel  ("\n"),
 
-    plotOutput("myPlot", width = "900px",height = "450px"),
+     plotOutput("myPlot", width = "900px",height = "450px"),
     
-    hr(),
-    hr(),
-    hr(),
-    hr(),
-    hr(),
+    br(),
+    br(),
+    br(),
+    br(),
+    br(),
     
     fluidRow(
-      column(3,
-             h4("Settings:"),
-      sliderInput("dotSize", "Select dot size",
-                  min=0.1, max=5, value = 2.1, step = 0.2), 
-      br(),
-     downloadButton(outputId="download_image",label = "Download plot")
-
-     
-    
-     
-     
-     
-      
-    ),
     column(4, offset = 1,
            radioButtons("Col_group_radio", "Color grouping by:",
                         c('Samples'= 'sample_id', 'Diagnosis'='type_id', 'Gene related'='gene_id'))    ),
     column(4,
            checkboxInput('jitter2', 'Jitter')
+    ),
+    column(3,
+           h3(span(strong(em("Settings:")), style = "color:#212F3D")),
+           br(),
+           sliderInput("dotSize", strong("Select dot size"),
+                       min=0.1, max=5, value = 2.1, step = 0.2), 
+           br(),
+           checkboxInput('background', strong('Guiding background')),
+           br(),
+           downloadButton(outputId="download_image",label = "Download plot")
     )
+    
     
     )))
     
-    #sidebarPanel  (
-#                    selectInput("Peaks", "please select peak type",
- #                               choices = c("Super peaks>=5 reads", "Normal peaks>=1 read") ) ,
-    #                sliderInput("dotSize", "Select sample size",
-     #                            min=1, max=10, value = 5, step = 1),
-  #                  conditionalPanel("input.Peaks=='Super peaks>=5 reads'",
-   #                                  textInput("Mean","please enter",5)),
-    #                conditionalPanel("input.Peaks=='Normal peaks>=1 read'",
-     #                                textInput("Max median","please enter",5),
-      #                               textInput("Min median","please enter",5)
-       #                              )
-     #               ),
-    
-    
-#    mainPanel (
- #     plotOutput("myPlot")
-  #  )
-    
-    
-#  )
-#)
-
-
-
